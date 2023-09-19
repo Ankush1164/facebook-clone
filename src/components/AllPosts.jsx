143b27aac48d4dc017e718d40c9a848059ec8593
Ankush1164 <ankushdhiman028@gmail.com>
2023-09-19 11:01:22 +0530

    nothing

--- src/components/AllPosts.jsx
+++ src/components/AllPosts.jsx
@@ -1,25 +1,45 @@
 import React, { useState, useEffect } from 'react'
+import "../components/Utility.css"
 
 
 function AllPosts() {
-    let url = `https://api.unsplash.com/search/photos?page=10&query=all&client_id=GWBM0Iz1Sm8mf5w6uYK2tuyIa9V2ZWIVnRgiIgyriEI`
     const [data, setData] = useState([]);
+    const [page, setPage] = useState(1);
+
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=all&client_id=GWBM0Iz1Sm8mf5w6uYK2tuyIa9V2ZWIVnRgiIgyriEI`;
+
+    // Fetch data from the API when the 'page' value changes.
     const fetchInfo = async () => {
-        return await fetch(url)
-            .then((res) => res.json())
-            .then((d) => setData(d.results))
-    }
-    console.log(data)
+        try {
+            const response = await fetch(url);
+            if (response.ok) {
+                const data = await response.json();
+                setData((prevData) => [...prevData, ...data.results]);
+            } else {
+                console.error('Failed to fetch data');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
     useEffect(() => {
         fetchInfo();
-    }, []);
-    return (
+    }, [page]);
+
+    // Function to load more data when the "See More" button is clicked.
+    function loadMoreData() {
+        setPage((prevPage) => prevPage + 1);
+    }
+
+
+        return (
         <div>
             {
                 data && data.length > 0 && data.map((dataObj) => {
                     return (
 
-                        <div className='w-full h-auto border-black border-2 mb-5 rounded-md'>
+                        <div className='w-full h-auto border-slate-500 border-2 mb-5 rounded-md'>
                             <div className='flex w-full gap-3 p-2 items-center'>
                                 <div className='bg-red-200 h-12 w-12 rounded-full'>
                                     <img src={dataObj.user.profile_image.medium} alt="" srcset="" className='rounded-full' />
@@ -37,10 +57,15 @@ function AllPosts() {
                             <div>
                                 <img src={dataObj.urls.regular} alt="" srcset="" />
                             </div>
-                            <div className="bg-blue-200 w-full h-24 flex gap-8 user_intrestion px-2 py-2">
-                                <div className='flex gap-2 items-center h-8 w-full border-b-2 border-slate-500'>
+                            <div className="w-full h-auto flex user_intrestion px-2 py-2 flex-col">
+                                <div className='flex gap-2 items-center h-8 w-full'>
                                 <span><i class="fa-regular fa-thumbs-up text-sm"></i></span>
                                 <p className='text-sm'>Liked by {dataObj.likes} pepole</p>
+                                </div>
+
+                                <div className='w-full h-auto flex justify-around p-1 comment_sec border-t-2 border-slate-400'>
+                                    <span><i class="fa-regular fa-thumbs-up"></i> Like</span>
+                                    <span><i class="fa-regular fa-comment"></i> Comment</span>
                                 </div>
 
                             </div>
@@ -50,10 +75,10 @@ function AllPosts() {
             }
 
             <div className=" fle w-full h-10 justify-center items-center">
-                <button className='border-2 border-current px-4'>See More</button>
+                <button className=' px-4 bg-blue-500' onClick={loadMoreData}>See More <i class="fa-solid fa-angle-down"></i></button>
             </div>
         </div>
     )
 }
-
 export default AllPosts
+
